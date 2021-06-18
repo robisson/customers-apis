753ac9a5e9cf1b1d7cbc8662b8328533ee6d9bfb
Robisson Oliveira <robisson@robissonoliveira.com.br>
2021-06-18 14:32:37 -0300

    Adjusting kubernetes deployment

--- src/customers/domain/services/customer.service.spec.ts
+++ src/customers/domain/services/customer.service.spec.ts
@@ -1,9 +1,19 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { CustomerService } from './customer.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { CreateCustomerDto } from '../../application/dto/create-customer.dto';
 import { ObjectID } from 'mongodb';
 import { Customer } from '../entities/customer.entity';
+
+const getModule = async (dynamicProviders): Promise<TestingModule> => {
+  return Test.createTestingModule({
+    providers: [
+      CustomerService,
+      ...dynamicProviders
+    ],
+  })
+    .compile();
+}
 
 describe('Customer Service', () => {
   let customerService: CustomerService;
@@ -15,28 +25,32 @@ describe('Customer Service', () => {
   }
 
   beforeEach(async () => {
-    const module = await Test.createTestingModule({
-      providers: [
-        CustomerService,
-        {
-          provide: getRepositoryToken(Customer),
-          useValue: {
-            save: () => ({ ...createCustomerDto, customer_id: new ObjectID() }),
-            findOne: () => {
-              findOne();
-              return new Customer();
-            },
-            find
-          }
-        }
-      ],
-    })
-      .compile();
+    const module = await getModule([{
+      provide: getRepositoryToken(Customer),
+      useValue: {
+        save: () => ({ ...createCustomerDto, customer_id: new ObjectID() }),
+        findOne: () => {
+          findOne();
+          return new Customer();
+        },
+        find
+      }
+    }]);
 
     customerService = await module.get(CustomerService);
   })
 
   it('create method should return Customer object', async () => {
+    const module = await getModule([{
+      provide: getRepositoryToken(Customer),
+      useValue: {
+        save: () => ({ ...createCustomerDto, customer_id: new ObjectID() }),
+        findOne,
+        find
+      }
+    }]);
+
+    customerService = await module.get(CustomerService);
     let customer: Customer = await customerService.create(createCustomerDto);
 
     expect(customer).toMatchObject(createCustomerDto);
@@ -47,7 +61,7 @@ describe('Customer Service', () => {
 
     expect(findOne).toBeCalled();
   })
-  
+
   it('findAll method should call find in the CustomerRepository', async () => {
     await customerService.findAll();
 
