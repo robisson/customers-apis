9058da24ca4f48402baa305ec9885e9bba34c8ea
Robisson Oliveira <robisson@robissonoliveira.com.br>
2021-06-18 16:14:29 -0300

    Add tests to customer controlller

--- src/customers/application/adapters/rest/customers.controller.spec.ts
+++ src/customers/application/adapters/rest/customers.controller.spec.ts
@@ -1,21 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersController } from './customers.controller';
 import { CustomerService } from '../../../domain/services/customer.service';
-import { Customer } from '../../../domain/entities/customer.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateCustomerDto } from '../../dto/create-customer.dto';
 
 describe('CustomersController', () => {
   let customersController: CustomersController;
+  const create = jest.fn();
+  const findAll = jest.fn();
+  const findOne = jest.fn();
+  const update = jest.fn();
+  const customer_id = '60cc830166b7c283fff08102';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomersController],
       providers: [
-        CustomerService,
         {
-          provide: getRepositoryToken(Customer),
+          provide: CustomerService,
           useValue: {
-            
+            create,
+            findAll,
+            findOne,
+            update
           }
         }
       ],
@@ -24,7 +30,29 @@ describe('CustomersController', () => {
     customersController = module.get<CustomersController>(CustomersController);
   });
 
-  it('should be defined', () => {
-    expect(customersController).toBeDefined();
+  it('Call create method should to call service create', () => {
+
+    customersController.create(new CreateCustomerDto());
+
+    expect(create).toBeCalledTimes(1);
+  });
+
+  it('Call findAll method should to call service create', () => {
+
+    customersController.findAll();
+
+    expect(findAll).toBeCalledTimes(1);
+  });
+  it('Call findOne method should to call service create', () => {
+
+    customersController.findOne(customer_id);
+
+    expect(findOne).toBeCalledTimes(1);
+  });
+  it('Call update method should to call service create', () => {
+
+    customersController.update(customer_id, new CreateCustomerDto());
+
+    expect(update).toBeCalledTimes(1);
   });
 });
