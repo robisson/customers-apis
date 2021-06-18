888c2be7e7bf4f5ffdf1feb6c872327ddc0b6e2c
Robisson Oliveira <robisson@robissonoliveira.com.br>
2021-06-18 15:23:48 -0300

    Add tests to auth service

--- src/auth/services/auth.service.spec.ts
+++ src/auth/services/auth.service.spec.ts
@@ -1,10 +1,20 @@
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../../users/services/user.service';
+import { UserDto } from '../dto/user.dto';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const access_token = {
+    access_token: 'hashtoken',
+  };
+
+  const userTologin: UserDto = {
+    email: "robisson@robissonoliveira.com.br",
+    password: "12345",
+    confirmPassword: "12345"
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,14 +23,14 @@ describe('AuthService', () => {
         {
           provide: JwtService,
           useValue: {
-            sign: () => false
+            sign: () => access_token.access_token
           }
         },
         {
           provide: UserService,
           useValue: {
             findUserByAndPassword: () => true,
-            save: () => false
+            save: () => userTologin
           }
         }
       ],
@@ -29,7 +39,21 @@ describe('AuthService', () => {
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('Login should to return a sign access_token', async () => {
+    const userTologin = {
+      username: "robisson@robissonoliveira.com.br",
+      password: "12345"
+    };
+
+    let user = await service.login(userTologin);
+
+    expect(user).toMatchObject(access_token);
+  });
+
+  it('Register a user should to return a user', async () => {
+
+    let user = await service.registerUser(userTologin);
+
+    expect(user).toMatchObject(userTologin);
   });
 });
--- src/products/domain/services/product.service.spec.ts
+++ src/products/domain/services/product.service.spec.ts
@@ -1,4 +1,4 @@
-import { Test } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
 import { ProductService } from './product.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { CreateProductDto } from '../dto/create-product.dto';
@@ -6,44 +6,67 @@ import { ObjectID } from 'mongodb';
 import { Product } from '../entities/product.entity';
 import { Customer } from '../../../customers/domain/entities/customer.entity';
 import { ProductCatalogService } from '../../../products/infrastructure/client/http/luiza-labs/product-catalog.service';
+import { CustomerService } from '../../../customers/domain/services/customer.service';
+
+const getModule = async (dynamicProviders): Promise<TestingModule> => {
+  return Test.createTestingModule({
+    providers: [
+      CustomerService,
+      ...dynamicProviders
+    ],
+  })
+    .compile();
+}
 
 describe('Product Service', () => {
   let productService: ProductService;
   let findOne: () => {} = jest.fn();
   let find: () => {} = jest.fn();
+  let findAndCount: () => {} = jest.fn();
   let createProductDto: CreateProductDto = {
     product_id: "some_id"
   }
 
   beforeEach(async () => {
-    const module = await Test.createTestingModule({
-      providers: [
-        ProductService,
-        ProductCatalogService,
-        {
-          provide: getRepositoryToken(Product),
-          useValue: {
-            save: () => ({ ...createProductDto, customer_id: new ObjectID() }),
-            findOne,
-            find
-          }
-        },
-        {
-          provide: getRepositoryToken(Customer),
-          useValue: {
-            findOne
+    const module = await getModule([
+      ProductService,
+      {
+        provide: ProductCatalogService,
+        useValue: {
+          getProductById: () => ({
+            id: '',
+            brand: '',
+            title: '',
+            image: '',
+          }),
+        }
+      },
+      {
+        provide: getRepositoryToken(Product),
+        useValue: {
+          save: () => ({ ...createProductDto, customer_id: new ObjectID() }),
+          findOne,
+          find,
+          findAndCount: () => {
+            findAndCount();
+            return [{}, {}];
           }
         }
-      ],
-    })
-      .compile();
+      },
+      {
+        provide: getRepositoryToken(Customer),
+        useValue: {
+          findOne: () => new Customer()
+        }
+      }
+    ])
 
     productService = await module.get(ProductService);
   })
 
   it('create method should return Product object', async () => {
 
-    const constomer_id: string = "some_id";
+    const constomer_id: string = '60cc830166b7c283fff08102';
 
     let customer: Product = await productService.create(constomer_id, createProductDto);
 
@@ -52,8 +75,40 @@ describe('Product Service', () => {
   })
 
   it('findOne method should call findOne in the ProductRepository', async () => {
+    const module = await getModule([
+      ProductService,
+      {
+        provide: ProductCatalogService,
+        useValue: {
+          getProductById: () => ({
+            id: '',
+            brand: '',
+            title: '',
+            image: '',
+          }),
+        }
+      },
+      {
+        provide: getRepositoryToken(Product),
+        useValue: {
+          save: () => ({ ...createProductDto, customer_id: new ObjectID() }),
+          findOne: () => {
+            findOne();
+            return new Product();
+          },
+          find
+        }
+      },
+      {
+        provide: getRepositoryToken(Customer),
+        useValue: {
+          findOne: () => new Customer()
+        }
+      }
+    ])
 
-    await productService.findOne('customer_id','product_id');
+    productService = await module.get(ProductService);
+    await productService.findOne('60cc830166b7c283fff08102', '60cc830166b7c283fff08102');
 
     expect(findOne).toBeCalled();
 
@@ -61,9 +116,9 @@ describe('Product Service', () => {
 
   it('findAll method should call find in the ProductRepository', async () => {
 
-    await productService.findAll('customer_id','1', '10');
+    await productService.findAll('60cc830166b7c283fff08102', '1', '10');
 
-    expect(find).toBeCalled();
+    expect(findAndCount).toBeCalled();
 
   })
 });
